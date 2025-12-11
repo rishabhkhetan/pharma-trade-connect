bce134db3ce4b426050e33681de8b77b27134f9f
Hemaraju <hemarajubora1@gmail.com>
2025-12-11 22:09:00 +0530

    Update router to include Admin PUT/DELETE /api/products/:id routes

--- backend/main.go
+++ backend/main.go
@@ -25,15 +25,19 @@ func main() {
 	protected := r.Group("/api")
 	protected.Use(AuthMiddleware()) // Function is in middleware.go
 	{
-		protected.GET("/products", getProductsHandler) // in products.go
-		protected.POST("/orders", placeOrderHandler)   // in orders.go
+		protected.GET("/products", getProductsHandler)          // in products.go
+		protected.POST("/orders", placeOrderHandler)            // in orders.go
+		protected.POST("/products", addProductHandler)          // Admin uses this to add products
+		protected.PUT("/products/:id", updateProductHandler)    // UPDATE <-- NEW
+		protected.DELETE("/products/:id", deleteProductHandler) // DELETE <-- NEW
 
 		// Admin Routes (in admin.go)
 		protected.GET("/clients", getPendingClientsHandler)
 		protected.POST("/admin/approve", approveClientHandler)
+
 	}
 
 	// 6. Start Server
-	fmt.Println("Server running on port 8080...")
-	r.Run(":8080")
+	fmt.Println("Server running on port 8081")
+	r.Run(":8081")
 }
